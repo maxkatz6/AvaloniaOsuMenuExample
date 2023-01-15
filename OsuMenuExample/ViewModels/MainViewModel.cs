1bc4fc9bed726f140e3d77253a20bd01132d211a
Max Katz <maxkatz6@outlook.com>
2023-01-14 21:28:21 -0500

    Add sample project

---
+++ OsuMenuExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OsuMenuExample;
+
+public class MainViewModel : ObservableObject
+{
+    public MainViewModel()
+    {
+        Maps = new ObservableCollection<MapViewModel>(new[]
+        {
+            new MapViewModel("S", "SHIORI", "ClariS", "Insane"),
+            new MapViewModel("A", "MONSTER", "Reol", "Expert"),
+            new MapViewModel("A", "Hall Of The Mountain King", "Apocalyptica", "Insane"),
+            new MapViewModel("S", "Chocolate", "GUMI", "Insane"),
+            new MapViewModel("S", "Frame of Mind", "Tristam", "Normal"),
+            new MapViewModel("B", "Sprinter", "Kalafina", "Insane"),
+            new MapViewModel("S", "Katana no Saya", "Ali Project", "Normal"),
+        });
+    }
+    
+    public IReadOnlyCollection<MapViewModel> Maps { get; }
+}
---
+++ OsuMenuExample/ViewModels/MapViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OsuMenuExample;
+
+public class MapViewModel : ObservableObject
+{
+    public MapViewModel(string rank, string title, string artist, string difficulty)
+    {
+        Rank = rank;
+        Title = title;
+        Artist = artist;
+        Difficulty = difficulty;
+    }
+    
+    public string Rank { get; }
+    public string Title { get; }
+    public string Artist { get; }
+    public string Difficulty { get; }
+}
